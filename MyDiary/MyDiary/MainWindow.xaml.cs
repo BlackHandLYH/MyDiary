e4666a78c9a678d50953847e7f1fdcf9d633bfa2
BlackHandLYH <549005769@qq.com>
2019-04-14 15:49:08 +0800

    Password function added using reg

--- MyDiary/MyDiary/MainWindow.xaml.cs
+++ MyDiary/MyDiary/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace MyDiary
 {
@@ -22,10 +23,12 @@ namespace MyDiary
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string original_text = "Record This Moment NOW...";
+        public string original_text = "记录这一刻吧...";
         public string record_text = "";
-        public string filepath = "D:/diary.txt";
+        public string filepath = "D:/MyDiary/diary.txt"; 
+        public string pword = "123456";
         int emotion = 0;  //0-happy 1-sad 2-angry
+        int win_now = 0;
         int R, G, B;
 
         public void UpdateEmotion()
@@ -51,7 +54,7 @@ namespace MyDiary
         {
             if (this.diaryBox.Text == original_text || this.diaryBox.Text == "")
             {
-                MessageBox.Show("At least write something...OK?", "Oops...", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("好歹记录点儿什么嘛...", "Oops...", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             else
             {
@@ -87,7 +90,7 @@ namespace MyDiary
                 fs.Flush();
                 fs.Close();
 
-                MessageBox.Show("You Have Recorded This Moment. :)");
+                MessageBox.Show("你已经记录下这一刻。 :)");
                 this.diaryBox.Text = original_text;
                 this.Label_emotion.Focus();
             }
@@ -106,6 +109,7 @@ namespace MyDiary
             SolidColorBrush myBrush = new SolidColorBrush(color);
             this.MyDiary.Background = myBrush;
         }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -118,6 +122,25 @@ namespace MyDiary
             string weekday_string = DateTime.Now.DayOfWeek.ToString();
             this.Label_time.Content = date_string + " " + weekday_string;
             this.diaryBox.Text = original_text;
+
+            WinChange(0);
+            
+            string help_msg = "";
+            help_msg += "MyDiary By BlackHand\n";
+            help_msg += "o : 打开日记\n";
+            help_msg += "c : 更换颜色\n";
+            help_msg += "×: 退出";
+            Label_helptext.Content = help_msg;
+
+            try
+            {
+                ManageReg();
+            }
+            catch
+            {
+                MessageBox.Show("请使用管理员权限打开本应用。");
+                Application.Current.Shutdown();
+            }
         }
 
         private void DiaryBox_GotFocus(object sender, RoutedEventArgs e)
@@ -200,7 +223,7 @@ namespace MyDiary
 
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            base.DragMove();
+            DragMove();
         }
 
         private void Label_close_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -250,12 +273,41 @@ namespace MyDiary
 
         private void Label_help_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            string help_msg = "";
-            help_msg += "MyDiary By BlackHand\n";
-            help_msg += "o : Open Diary By Notepad++\n";
-            help_msg += "c : Change Background Color\n";
-            help_msg += "×: Exit";
-            MessageBox.Show(help_msg, "Help", MessageBoxButton.OK, MessageBoxImage.Question);
+            switch (win_now)
+            {
+                case 0:
+                    WinChange(2);
+                    break;
+                case 1:
+                    WinChange(2);
+                    break;
+                case 2:
+                    WinChange(0);
+                    break;
+                case 3:
+                    WinChange(2);
+                    break;
+                default:
+                    WinChange(0);
+                    break;
+            }
+        }
+
+        private void PwBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if(PwBox.Password == pword)
+                {
+                    WinChange(0);
+                    System.Diagnostics.Process.Start(filepath);
+                }
+                else
+                {
+                    PwBox.Password = "";
+                    MessageBox.Show("密码错误！", "Wrong!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
         private void Label_open_MouseLeave(object sender, MouseEventArgs e)
@@ -265,10 +317,211 @@ namespace MyDiary
 
         private void Label_open_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            System.Diagnostics.ProcessStartInfo Info = new System.Diagnostics.ProcessStartInfo();
-            Info.FileName = "Notepad++.exe";
-            Info.Arguments = filepath;
-            System.Diagnostics.Process Proc = System.Diagnostics.Process.Start(Info);
+            switch(win_now)
+            {
+                case 0:
+                    WinChange(1);
+                    break;
+                case 1:
+                    WinChange(0);
+                    break;
+                case 2:
+                    WinChange(1);
+                    break;
+                case 3:
+                    WinChange(1);
+                    break;
+                default:
+                    WinChange(0);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 选择当前有效窗口
+        /// </summary>
+        /// <param name="win_id">0-ui 1-pw 2-help</param>
+        public void WinChange(int win_id)
+        {
+            PwBox.Password = "";
+            OldPwBox.Password = "";
+            NewPwBox.Password = "";
+            if (win_id == 0)
+            {
+                win_now = 0;
+                Grid_ui.IsEnabled = true;
+                Grid_ui.Visibility = Visibility.Visible;
+                Grid_pw.IsEnabled = false;
+                Grid_pw.Visibility = Visibility.Hidden;
+                Grid_help.IsEnabled = false;
+                Grid_help.Visibility = Visibility.Hidden;
+                Grid_changepw.IsEnabled = false;
+                Grid_changepw.Visibility = Visibility.Hidden;
+            }
+            else if(win_id == 1)
+            {
+                win_now = 1;
+                Grid_ui.IsEnabled = false;
+                Grid_ui.Visibility = Visibility.Hidden;
+                Grid_pw.IsEnabled = true;
+                Grid_pw.Visibility = Visibility.Visible;
+                Grid_help.IsEnabled = false;
+                Grid_help.Visibility = Visibility.Hidden;
+                Grid_changepw.IsEnabled = false;
+                Grid_changepw.Visibility = Visibility.Hidden;
+            }
+            else if(win_id == 2)
+            {
+                win_now = 2;
+                Grid_ui.IsEnabled = false;
+                Grid_ui.Visibility = Visibility.Hidden;
+                Grid_pw.IsEnabled = false;
+                Grid_pw.Visibility = Visibility.Hidden;
+                Grid_help.IsEnabled = true;
+                Grid_help.Visibility = Visibility.Visible;
+                Grid_changepw.IsEnabled = false;
+                Grid_changepw.Visibility = Visibility.Hidden;
+            }
+            else if(win_id == 3)
+            {
+                win_now = 3;
+                Grid_ui.IsEnabled = false;
+                Grid_ui.Visibility = Visibility.Hidden;
+                Grid_pw.IsEnabled = false;
+                Grid_pw.Visibility = Visibility.Hidden;
+                Grid_help.IsEnabled = false;
+                Grid_help.Visibility = Visibility.Hidden;
+                Grid_changepw.IsEnabled = true;
+                Grid_changepw.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Label_changepw_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            switch (win_now)
+            {
+                case 0:
+                    WinChange(3);
+                    break;
+                case 1:
+                    WinChange(3);
+                    break;
+                case 2:
+                    WinChange(3);
+                    break;
+                case 3:
+                    WinChange(0);
+                    break;
+                default:
+                    WinChange(0);
+                    break;
+            }
+        }
+
+        private void Label_changepwenter_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ChangePW();
+        }
+
+        public void ManageReg()
+        {
+            RegistryKey key = Registry.LocalMachine;
+            RegistryKey key_md;
+            if (!IsExistReg("SOFTWARE", "MyDiary"))
+                key_md = key.CreateSubKey("SOFTWARE\\MyDiary");
+            else
+                key_md = key.OpenSubKey("SOFTWARE\\MyDiary", true);
+            if(IsExistRegValue("SOFTWARE\\MyDiary", "FILEPATH"))
+            {
+                filepath = key_md.GetValue("FILEPATH").ToString();
+            }
+            else
+            {
+                key_md.SetValue("FILEPATH", filepath, RegistryValueKind.String);
+            }
+            if (IsExistRegValue("SOFTWARE\\MyDiary", "PASSWORD"))
+            {
+                pword = key_md.GetValue("PASSWORD").ToString();
+            }
+            else
+            {
+                key_md.SetValue("PASSWORD", pword, RegistryValueKind.String);
+            }
+            key_md.Close();
+        }
+
+        public bool IsExistReg(string path, string keyword)
+        {
+            string[] subkeyNames;
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey software = hkml.OpenSubKey(path);
+            subkeyNames = software.GetSubKeyNames();
+            //取得该项下所有子项的名称的序列，并传递给预定的数组中  
+            foreach (string keyName in subkeyNames)
+            //遍历整个数组  
+            {
+                if (keyName == keyword)
+                //判断子项的名称  
+                {
+                    hkml.Close();
+                    return true;
+                }
+            }
+            hkml.Close();
+            return false;
+        }
+
+        private void NewPwBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ChangePW();
+            }
+        }
+
+        public bool IsExistRegValue(string path, string keyvalue)
+        {
+            string[] subkeyNames;
+            RegistryKey hkml = Registry.LocalMachine;
+            RegistryKey software = hkml.OpenSubKey(path);
+            //RegistryKey software = hkml.OpenSubKey("SOFTWARE\\test", true);
+            subkeyNames = software.GetValueNames();
+            //取得该项下所有键值的名称的序列，并传递给预定的数组中
+            foreach (string keyName in subkeyNames)
+            {
+                if (keyName == keyvalue) //判断键值的名称
+                {
+                    hkml.Close();
+                    return true;
+                }
+
+            }
+            hkml.Close();
+            return false;
+        }
+
+        public void ChangePW()
+        {
+            if (OldPwBox.Password == "" || NewPwBox.Password == "")
+            {
+                MessageBox.Show("密码不能为空！");
+                return;
+            }
+            else if (OldPwBox.Password != pword)
+            {
+                MessageBox.Show("原密码错误！");
+                return;
+            }
+            else
+            {
+                pword = NewPwBox.Password;
+                RegistryKey key = Registry.LocalMachine;
+                RegistryKey key_md = key.OpenSubKey("SOFTWARE\\MyDiary", true);
+                key_md.SetValue("PASSWORD", pword);
+                MessageBox.Show("修改成功");
+                WinChange(2);
+                key_md.Close();
+            }
         }
     }
 }
